5f756cd72686dc0d0be33f3df6bf5ce14067db78
mc_memory@naver.com <mc_memory@naver.com>
2020-03-02 16:43:37 +0900

    Git Tutorial

--- src/Git_Test.java
+++ src/Git_Test.java
@@ -4,7 +4,7 @@ public class Git_Test {
 	public static void main(String[] args) {
 		System.out.println("Testing Git Hub");
 		System.out.println("Retesting Git Hub");
-		System.out.println("Hello World");
+		System.out.println("Hello World!!");
 	}
 
 }
